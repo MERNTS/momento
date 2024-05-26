8b113d56a2b058ca13a9b05754148d28d4e33cf2
aiXpertLab <aiXpertLab@gmail.com>
2024-05-25 20:04:05 -0400

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './assets/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite Momento</h1>
+      <h1>My My</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 // deploy to azure, key thing is : app folder: /, output folder: /dist
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
+import './assets/index.css'
 import App from './App.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
 )
